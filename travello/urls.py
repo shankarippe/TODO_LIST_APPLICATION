75b3f7a1843c08d3dd17a7e99d73ef99d8a4c5da
shankarippe <shankarippe23@gmail.com>
2025-03-24 22:55:38 +0530

    Delete travello directory

--- travello/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # Import views from the same directory
-
-urlpatterns = [
-    path('', views.index, name='home'),  # This makes 'travello/' work
-]
